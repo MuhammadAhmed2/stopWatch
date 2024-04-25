56c19982277d8b841887e4ca8ce4bd83bf87b7e6
MuhammadAhmed2 <exact.ahmed@gmail.com>
2024-04-25 18:05:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let s1 = document.querySelector("#s1");
+let m1 = document.querySelector("#m1");
+let h1 = document.querySelector("#h1");
+let sec = 0;
+let min = 0;
+let hrs = 0;
+
+function timer() {
+    tikTik = setInterval(function() {    
+       
+        if (sec >= 59) {
+            sec = 0;
+            min += 1;
+            m1.innerHTML = min;
+        }
+        if(min >= 59){
+            min = 0;
+            hrs += 1;
+            h1.innerHTML = hrs;
+        }
+        sec += 1;
+        s1.innerHTML= sec;
+    },1000)
+}
+
+function Stop() {
+    clearInterval(tikTik)
+}
+
+function reset() {
+ sec = 0; min = 0; hrs = 0;
+ s1.innerHTML = 0;
+ m1.innerHTML = 0;
+ h1.innerHTML = 0;
+}
